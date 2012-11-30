dc5e5b9591cd801409189def55d01fca845dd5eb
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-01 00:22:28 +0800

    tool renaming

--- pdc.bat
+++ pdc.bat
@@ -1,5 +1,5 @@
 @echo off
-rem Packfire Framework Dependency Checker (pdc)
+rem Packfire Dependency Checker (pdc)
 rem Written by Sam-Mauris Yong
 rem Released Open Source under New BSD 3-Clause License
 rem Visit us at http://mauris.sg/packfire
