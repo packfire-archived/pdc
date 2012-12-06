d4ff45a8b36582ee4e56d8edee43729004219dc5
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-07 00:15:11 +0800

    analyzer partial implementation

--- src/Packfire/PDC/Analyzer.php
+++ src/Packfire/PDC/Analyzer.php
@@ -20,6 +20,62 @@ namespace Packfire\PDC;
  * @since 1.0.4
  * @link https://github.com/packfire/pdc/
  */
-class Analyzer{
+class Analyzer {
+
+	/**
+	 * PHP source code tokens
+	 * @var array
+	 * @since 1.0.4
+	 */
+	private $tokens;
+
+	/**
+	 * The current processing token index
+	 * @var integer
+	 * @since 1.0.4
+	 */
+	private $current;
+
+	/**
+	 * The cache of number of tokens
+	 * @var integer
+	 * @since 1.0.4
+	 */
+	private $count;
+
+	/**
+	 * Create a new Analyzer object
+	 * @param string $source The source code to analyze
+	 * @since 1.0.4
+	 */
+	public function __construct($source){
+    	$this->tokens = token_get_all($php_code);
+    	$this->count = count($tokens);
+	}
+
+	/**
+	 * Get the full namespace / classname
+	 * @param integer &$start (reference) The start index to read
+	 *		 the Namespace/Classname from
+	 * @return string Returns the full namespace or classname read
+	 * @since 1.0.4
+	 */
+	private function fullClass(&$start){
+	    $class = '';
+	    do{
+	        if(is_array($this->tokens[$start])){
+	            $current = $this->tokens[$start][0];
+	            if($current == T_NS_SEPARATOR || $current == T_STRING 
+	            	|| $current == T_VARIABLE || $current == T_STATIC){
+	                $class .= $this->tokens[$start][1];
+	            }else{
+	                break;
+	            }
+	        }else{
+	            break;
+	        }
+	    }while(++$start < $this->count);
+	    return $class;
+	}
 
 }
