7c663ae22e0ce74631bfbd96c558302d94aae0bb
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-06 23:08:13 +0800

    class implementations

---
+++ src/Packfire/PDC/PDC.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Packfire Dependency Checker (pdc)
+ * Written by Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Packfire\PDC;
+
+/**
+ * The main PDC class for running and processing the source code
+ * 
+ * @author Sam-Mauris Yong <sam@mauris.sg>
+ * @copyright 2012 Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * @license http://www.opensource.org/licenses/BSD-3-Clause The BSD 3-Clause License
+ * @package Packfire\PDC
+ * @since 1.0.4
+ * @link https://github.com/packfire/pdc/
+ */
+class PDC{
+
+}
