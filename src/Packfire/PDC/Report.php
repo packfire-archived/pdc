cd9d557853a70c1e343e83dc56300fb5875f522a
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-09 07:14:42 +0800

    empty implementation of Report

---
+++ src/Packfire/PDC/Report.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Packfire Dependency Checker (pdc)
+ * By Sam-Mauris Yong
+ * 
+ * Released open source under New BSD 3-Clause License.
+ * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * All rights reserved.
+ */
+
+namespace Packfire\PDC;
+
+/**
+ * Report holding object
+ * 
+ * @author Sam-Mauris Yong <sam@mauris.sg>
+ * @copyright 2012 Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * @license http://www.opensource.org/licenses/BSD-3-Clause The BSD 3-Clause License
+ * @package Packfire\PDC
+ * @since 1.0.4
+ * @link https://github.com/packfire/pdc/
+ */
+class Report {
+
+	
+}
