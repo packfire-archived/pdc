3ba8edf46a91248d702833dea2af0a6cba6ba49c
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-09 07:20:48 +0800

    moved Report into its own namespace

--- src/Packfire/PDC/Report/Report.php
+++ src/Packfire/PDC/Report/Report.php
@@ -8,7 +8,7 @@
  * All rights reserved.
  */
 
-namespace Packfire\PDC;
+namespace Packfire\PDC\Report;
 
 /**
  * Report holding object
@@ -16,7 +16,7 @@ namespace Packfire\PDC;
  * @author Sam-Mauris Yong <sam@mauris.sg>
  * @copyright 2012 Sam-Mauris Yong Shan Xian <sam@mauris.sg>
  * @license http://www.opensource.org/licenses/BSD-3-Clause The BSD 3-Clause License
- * @package Packfire\PDC
+ * @package Packfire\PDC\Report
  * @since 1.0.4
  * @link https://github.com/packfire/pdc/
  */
