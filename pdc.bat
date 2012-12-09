6458e0d390bab3c54d9b91abdf8e780b5e24cf0c
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-09 14:02:11 +0800

    removed old binaries

--- pdc.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-rem Packfire Dependency Checker (pdc)
-rem Written by Sam-Mauris Yong
-rem Released Open Source under New BSD 3-Clause License
-rem Visit us at http://mauris.sg/packfire
-php "%~dp0pdc" %*
