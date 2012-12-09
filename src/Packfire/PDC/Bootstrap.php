518c949b0eeb1ba181a3d30350e28c12fc009a39
Sam-Mauris Yong <mauris@hotmail.sg>
2012-12-09 14:00:29 +0800

    whitespace fix

--- src/Packfire/PDC/Bootstrap.php
+++ src/Packfire/PDC/Bootstrap.php
@@ -21,25 +21,23 @@ namespace Packfire\PDC;
  * @since 1.0.4
  * @link https://github.com/packfire/pdc/
  */
+class Bootstrap {
 
-class Bootstrap{
+    private static function acquire($file) {
+        if (is_file($file)) {
+            return include($file);
+        }
+    }
 
-	private static function acquire($file){
-		if(is_file($file)){
-			return include($file);
-		}
-	}
-
-	public static function run(){
-		if(!($loader = self::acquire(__DIR__ . '/../../../vendor/autoload.php'))){
-			echo 'You must set up project\'s dependencies first by running the following commands:' . PHP_EOL;
-			echo "    curl -s https://getcomposer.org/installer | php\n";
-			echo "    php composer.phar install\n";
-			exit(1);
-
-		}
-		return $loader;
-	}
+    public static function run() {
+        if (!($loader = self::acquire(__DIR__ . '/../../../vendor/autoload.php'))) {
+            echo 'You must set up project\'s dependencies first by running the following commands:' . PHP_EOL;
+            echo "    curl -s https://getcomposer.org/installer | php\n";
+            echo "    php composer.phar install\n";
+            exit(1);
+        }
+        return $loader;
+    }
 
 }
 
