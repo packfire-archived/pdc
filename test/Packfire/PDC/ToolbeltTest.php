5c15a7b271c7855c1ba689de8ff7c1c6a879fa4f
Sam-Mauris Yong <mauris@hotmail.sg>
2013-01-02 20:48:08 +0800

    FileTest skeleton generated
    test updates

--- test/Packfire/PDC/ToolbeltTest.php
+++ test/Packfire/PDC/ToolbeltTest.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Packfire Dependency Checker (pdc)
  * By Sam-Mauris Yong
  * 
  * Released open source under New BSD 3-Clause License.
- * Copyright (c) 2012, Sam-Mauris Yong Shan Xian <sam@mauris.sg>
+ * Copyright (c) Sam-Mauris Yong Shan Xian <sam@mauris.sg>
  * All rights reserved.
  */
 
